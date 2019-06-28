6f7c1d9deb008e0c81656769b766aedd3d64370b
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2019-06-28 17:34:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
-import os
 import telebot
-import time
-import random
-import threading
-from emoji import emojize
-from telebot import types
-from pymongo import MongoClient
-import traceback
 
-token = os.environ['TELEGRAM_TOKEN']
-bot = telebot.TeleBot(token)
+bot = telebot.Telebot("876526827:AAGWgaPYbiFWLF9PDdoGI8ZeMzJtqoJCVtE")
 
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+    bot.reply_to(message, message.text)
 
-client=MongoClient(os.environ['database'])
-db=client.
-users=db.users
-
-
-def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
-    return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
-                                 parse_mode=parse_mode)   
-
-print('7777')
-bot.polling(none_stop=True,timeout=600)
-
+bot.polling(none_stop = True)
