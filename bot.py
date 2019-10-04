bd41751b184ba31fab16324560e3a2fbe681ceca
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2019-10-04 19:48:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,34 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def text(m):
-	if m.text.lower[:6]=='!кража':
-		bot.send_message(m, m.from_user.first_name+" совершает кражу"+m.text[5:])
+	if m.text[:6].lower()=='!кража':
+		if m.from_user.id not in steal2_wait:
+			chance = random.randint(1, 100)
+			if random.randint(1, 100)<=chance:
+				success = 1
+			else:
+				success = 0
+			t1 = m.text.split()
+			i = 1
+			target = ''
+			while i<len(t1):
+				if i + 1 == len(t1):
+					target += t1[i]
+				else:
+					target += t1[i]+' '
+				i += 1
+			target = target.replace('*', '').replace('_', '').replace('`', '')
+			target2 = None
+			if target == '':
+				return
+			text = '*' + name + '* успешно реализует кражу *' + target + '! Шанс на успех был целых ' + str(chance) + ' процентов!'
+			if success == 1:
+				bot.send_message(m.chat.id, text, parse_mode = 'markdown')
+			else:
+				bot.send_message(m.chat.id, 'У *' + name + '* не хватает уменияреализовать кражу *' + target + '*! А ведь шанс был целых 'str(chance) + ' процентов!')
+			steal2_wait.update({m.from_user.id:{'timer'=60}})
+		else:
+			secs = steal2_wait[user['id']]['timer']
+			bot.send_message(m.chat.id, '*' + name + '*, вы сможете красть через ' + str(secs) + ' секунд.', parse_mode = 'markdown')
 
 bot.polling(none_stop=True)
