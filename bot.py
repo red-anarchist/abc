46ee63190c20f5e0d9a4685f73837476c47df97a
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2019-10-04 16:41:34 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import telebot
 
-bot = telebot.TeleBot("876526827:AAGWgaPYbiFWLF9PDdoGI8ZeMzJtqoJCVtE")
+bot = telebot.TeleBot('942081274:AAF4q7sOVQYb_q9uF_bg6OXuFnFMzL1u4a8')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 @bot.message_handler(content_types=['text'])
-def send_echo(message):
-    bot.reply_to(message, message.text)
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
 
-bot.polling(none_stop = True)
+bot.polling(none_stop=True)
