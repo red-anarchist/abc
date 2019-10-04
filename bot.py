95f56aa45cd5bb145adb5b2e87d2b2876b1869dd
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2019-10-04 17:30:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,16 @@ bot = telebot.TeleBot('981943532:AAHI9aAZkRG4Q0UuNIy2wKtve_y9QFMf9FU')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Д')
+    bot.send_message('Привет!')
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+
     if message.text.lower() == 'привет':
-        bot.send_message('Привет, мой создатель')
+        bot.send_message('Здарова!')
     elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
+        bot.send_message('Прощай, создатель')
+    elif message.text[0] == '!':
+        bot.send_message('хочет '+send_message[1:-1])
 
 bot.polling(none_stop=True)
