6e072a3b4b9799d61f7ce5dc26968f0833124e8e
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2020-01-12 18:56:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,74 @@
-# -*- coding: utf-8 -*-
 import telebot
 import random
 
-bot = telebot.TeleBot('981943532:AAHI9aAZkRG4Q0UuNIy2wKtve_y9QFMf9FU')
+bot = telebot.TeleBot('1032705208:AAHL8Ut4k7iybsTEMszWVFB2JA8_Hw7GsBQ')
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message('Привет!')
+KNB_list = ['камень', 'ножницы', 'бумага']
 
-steal_wait = {}
-steal2_wait = {}
+def ran(p_list):
+	index = random.randint(0, int(len(p_list))-1)
+	return p_list[index]
 
 @bot.message_handler(content_types=['text'])
-def text(m):
-	name = m.from_user.first_name
-	if m.text[:6].lower()=='!кража':
-		if m.from_user.id not in steal2_wait:
-			chance = random.randint(1, 100)
-			if random.randint(1, 100)<=chance:
-				success = 1
-			else:
-				success = 0
-			t1 = m.text.split()
-			i = 1
-			target = ''
-			while i<len(t1):
-				if i + 1 == len(t1):
-					target += t1[i]
+def get_text_messages(message):
+	mes = message.text.lower().split()
+	M = list(message.text)
+
+	if M[0] == '!' or M[0] == '/':
+		if mes[0] == '!игра':
+			KNB = ran(KNB_list)
+			if len(mes) > 1:
+
+				if mes[1] == 'к' or mes[1] == 'камень':
+					if KNB == 'камень':
+						bot.send_message(message.chat.id, 'Ничья.')
+					elif KNB == 'ножницы':
+						bot.send_message(message.chat.id, f'{message.from_user.first_name} победил.')
+					else:
+						bot.send_message(message.chat.id, f'{message.from_user.first_name} проиграл.')
+
+				elif mes[1] == 'н' or mes[1] == 'ножницы':
+					if KNB == 'камень':
+						bot.send_message(message.chat.id, f'{message.from_user.first_name} проиграл.')
+					elif KNB == 'ножницы':
+						bot.send_message(message.chat.id, 'Ничья.')
+					else:
+						bot.send_message(message.chat.id, f'{message.from_user.first_name} победил.')
+
+				elif mes[1] == 'б' or mes[1] == 'бумага':
+					if KNB == 'камень':
+						bot.send_message(message.chat.id, f'{message.from_user.first_name} победил.')
+					elif KNB == 'ножницы':
+						bot.send_message(message.chat.id, f'{message.from_user.first_name} проиграл.')
+					else:
+						bot.send_message(message.chat.id, 'Ничья.')
 				else:
-					target += t1[i]+' '
-				i += 1
-			target = target.replace('*', '').replace('_', '').replace('`', '')
-			target2 = None
-			if target == '':
-				return
-			text = '*' + name + '* успешно реализует кражу *' + target + '! Шанс на успех был целых ' + str(chance) + ' процентов!'
-			if success == 1:
-				bot.send_message(m.chat.id, text, parse_mode = 'markdown')
+					bot.send_message(message.chat.id, 'Надо добавить "К", "Н" или "Б".')
+
 			else:
-				bot.send_message(m.chat.id, 'У *' + name + '* не хватает умения реализовать кражу *' + target + '*! А ведь шанс был целых ' + str(chance) + ' процентов!')
-			steal2_wait.update({m.from_user.id:{'timer':60}})
+				bot.send_message(message.chat.id, 'Выбери камень (К), ножницы (Н) или бумагу (Б)\nПример: !игра к')
+
+		elif mes[0] == '!кража':
+			dstr = ''
+			for a in mes:
+				if a == mes[0]:
+					pass
+				else:
+					if a == mes[-1]:
+						dstr = dstr + a
+					else:
+						dstr = dstr + a + ' '
+			shans = random.randint(0, 100)
+			i = random.randint(0, 100)
+			if i <= shans:
+				bot.send_message(message.chat.id, f'{message.from_user.first_name} успешно реализует кражу {dstr}! Шанс на успех был {shans}%!')
+			else:
+				bot.send_message(message.chat.id, f'{message.from_user.first_name} не смог совершить кражу {dstr}, хотя шанс был {shans}%.')
+
+		elif message.text == '/help':
+			bot.send_message(message.chat.id, '"!игра (буква)" - камень ножницы бумага\n"!кража (предмет)" - попытка кражи')
+
 		else:
-			secs = steal2_wait[user['id']]['timer']
-			bot.send_message(m.chat.id, '*' + name + '*, вы сможете красть через ' + str(secs) + ' секунд.', parse_mode = 'markdown')
+			bot.send_message(message.chat.id, 'Неизвестная команда. Напиши /help.')
 
 bot.polling(none_stop=True)
