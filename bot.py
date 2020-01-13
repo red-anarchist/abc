89cd4596fc166872c5b42f245df3ee997ae212e0
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2020-01-13 21:39:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,74 +1,94 @@
+# Импорт
 import telebot
 import random
 
+# Токен
 bot = telebot.TeleBot('1032705208:AAHL8Ut4k7iybsTEMszWVFB2JA8_Hw7GsBQ')
 
-KNB_list = ['камень', 'ножницы', 'бумага']
+# Списки
+KNB_list = ['камень', 'ножницы', 'бумага'] # Игра
 
+trusiki_list = ['трусы-шорты', 'плавки', 'панталоны', 'слип', 'кюлот', 'бикини', 'чикибоксеры', 'стринги', 'семейники', 'боксеры', 'хипсы', 'джокстрэпы', 'брифы', 'трусы']
+
+# Случайный элемент списка
 def ran(p_list):
 	index = random.randint(0, int(len(p_list))-1)
 	return p_list[index]
 
+# Хендлер
 @bot.message_handler(content_types=['text'])
+
+# Главная функция
 def get_text_messages(message):
-	mes = message.text.lower().split()
-	M = list(message.text)
+	mes = message.text.split() # Текст сообщения разбитый на списки
 
-	if M[0] == '!' or M[0] == '/':
-		if mes[0] == '!игра':
-			KNB = ran(KNB_list)
-			if len(mes) > 1:
+	# Игра
+	if mes[0] == '!игра':
+		KNB = ran(KNB_list)
+		if len(mes) > 1:
 
-				if mes[1] == 'к' or mes[1] == 'камень':
-					if KNB == 'камень':
-						bot.send_message(message.chat.id, 'Ничья.')
-					elif KNB == 'ножницы':
-						bot.send_message(message.chat.id, f'{message.from_user.first_name} победил.')
-					else:
-						bot.send_message(message.chat.id, f'{message.from_user.first_name} проиграл.')
-
-				elif mes[1] == 'н' or mes[1] == 'ножницы':
-					if KNB == 'камень':
-						bot.send_message(message.chat.id, f'{message.from_user.first_name} проиграл.')
-					elif KNB == 'ножницы':
-						bot.send_message(message.chat.id, 'Ничья.')
-					else:
-						bot.send_message(message.chat.id, f'{message.from_user.first_name} победил.')
-
-				elif mes[1] == 'б' or mes[1] == 'бумага':
-					if KNB == 'камень':
-						bot.send_message(message.chat.id, f'{message.from_user.first_name} победил.')
-					elif KNB == 'ножницы':
-						bot.send_message(message.chat.id, f'{message.from_user.first_name} проиграл.')
-					else:
-						bot.send_message(message.chat.id, 'Ничья.')
+			if mes[1] == 'к' or mes[1] == 'камень':
+				if KNB == 'камень':
+					bot.send_message(message.chat.id, '*показываю камень*\nНичья.')
+				elif KNB == 'ножницы':
+					bot.send_message(message.chat.id, f'*показываю ножницы*\n{message.from_user.first_name} победил.')
 				else:
-					bot.send_message(message.chat.id, 'Надо добавить "К", "Н" или "Б".')
+					bot.send_message(message.chat.id, f'*показываю бумагу*\n{message.from_user.first_name} проиграл.')
 
-			else:
-				bot.send_message(message.chat.id, 'Выбери камень (К), ножницы (Н) или бумагу (Б)\nПример: !игра к')
-
-		elif mes[0] == '!кража':
-			dstr = ''
-			for a in mes:
-				if a == mes[0]:
-					pass
+			elif mes[1] == 'н' or mes[1] == 'ножницы':
+				if KNB == 'камень':
+					bot.send_message(message.chat.id, f'*показываю камень*\n{message.from_user.first_name} проиграл.')
+				elif KNB == 'ножницы':
+					bot.send_message(message.chat.id, '*показываю ножницы*\nНичья.')
 				else:
-					if a == mes[-1]:
-						dstr = dstr + a
-					else:
-						dstr = dstr + a + ' '
-			shans = random.randint(0, 100)
-			i = random.randint(0, 100)
-			if i <= shans:
-				bot.send_message(message.chat.id, f'{message.from_user.first_name} успешно реализует кражу {dstr}! Шанс на успех был {shans}%!')
-			else:
-				bot.send_message(message.chat.id, f'{message.from_user.first_name} не смог совершить кражу {dstr}, хотя шанс был {shans}%.')
+					bot.send_message(message.chat.id, f'*показываю бумагу*\n{message.from_user.first_name} победил.')
 
-		elif message.text == '/help':
-			bot.send_message(message.chat.id, '"!игра (буква)" - камень ножницы бумага\n"!кража (предмет)" - попытка кражи')
+			elif mes[1] == 'б' or mes[1] == 'бумага':
+				if KNB == 'камень':
+					bot.send_message(message.chat.id, f'*показываю камень*\n{message.from_user.first_name} победил.')
+				elif KNB == 'ножницы':
+					bot.send_message(message.chat.id, f'*показываю ножницы*\n{message.from_user.first_name} проиграл.')
+				else:
+					bot.send_message(message.chat.id, '*показываю бумагу*\nНичья.')
+			else:
+				bot.send_message(message.chat.id, 'Надо добавить "К", "Н" или "Б".')
 
 		else:
-			bot.send_message(message.chat.id, 'Неизвестная команда. Напиши /help.')
+			bot.send_message(message.chat.id, 'Выбери камень (К), ножницы (Н) или бумагу (Б)\nПример: !игра к; !игра камень')
+
+	# Кража
+	elif mes[0] == '!кража':
+		dstr = message.text.lstrip('!кража ')
+		shans = random.randint(0, 100)
+		i = random.randint(0, 100)
+		if i <= shans:
+			bot.send_message(message.chat.id, f'{message.from_user.first_name} успешно реализует кражу {dstr}! Шанс на успех был {shans}%!')
+		else:
+			bot.send_message(message.chat.id, f'{message.from_user.first_name} не смог совершить кражу {dstr}, хотя шанс был {shans}%.')
+
+	# Кража трусов
+	elif mes[0] == '!трусики':
+		dstr = message.text.lstrip('!трусики ')
+		shans = random.randint(0, 100)
+		i = random.randint(0, 100)
+		if i <= shans:
+			bot.send_message(message.chat.id, f'{message.from_user.first_name} смог украсть {ran(trusiki_list)} у {dstr}! Шанс на успех был целых {shans}%!')
+		else:
+			bot.send_message(message.chat.id, f'{message.from_user.first_name} не смог украсть нижнее бельё {dstr}, каков извращенец! Шанс на успех был {shans}%.')
+
+	# Обновления
+	elif mes[0] == '/update' or '/обновления':
+		bot.send_message(message.chat.id, '*Последние обновления*\n \n*13.01.2020*\n--Исправлен текст игры\n--Добавлены обновления\n--Обновлено меню помощи\n--Упрощён код\n--Теперь можно красть трусики', parse_mode='markdown')
+
+	# Помощь
+	elif mes[0] == '/help' or mes[0] == '/помощь':
+		bot.send_message(message.chat.id, '*!игра* _(буква или объект)_ -- камень ножницы бумага\n*!кража* _(предмет в родительном падеже)_ -- попытка кражи\n*/update* `или` */обновления* -- показать последние обновления\n*!трусики* _(имя пользователя в родительном падеже)_ -- кража нижнего белья\n */help* `или` */помощь* -- показать все доступные функции', parse_mode='markdown')
+
+	# Неизвестная команада
+	elif message.text[0] == '!' or message.text[0] == '/':
+		bot.send_message(message.chat.id, 'Неизвестная команда, в /help (/помощь) вы можете посмотреть доступные команды.')
+
+	else:
+		pass
 
 bot.polling(none_stop=True)
