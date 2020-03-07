d094fa4544647ac0f27037e0c7fa1bd6c18c6dad
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2020-03-07 16:53:58 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,14 +6,17 @@ bot = telebot.TeleBot('1032705208:AAHL8Ut4k7iybsTEMszWVFB2JA8_Hw7GsBQ')
 @bot.message_handler(content_types=['text'])
 
 def get_text_messages(message):
+	if len(message.text) > 30:
+		text = f'{message.text[0:30]}...'
+	else:
+		text = message.text
+	print(f'{message.from_user.username}: "{text}"')
 	if message.text == 'Удивите меня':
 		bot.delete_message(message.chat.id, message.message_id)
 	elif message.text == '▶️ Игры':
 		bot.delete_message(message.chat.id, message.message_id)
 	elif message.text == 'Связаться с поддержкой':
 		bot.delete_message(message.chat.id, message.message_id)
-	elif message.text.lower() == 'del' and (message.from_user.username == 'ever_soup' or message.from_user.username == '@ever_soup'):
-		bot.delete_message(message.chat.id, message.reply_to_message.id)
 	elif message.text[:3].lower() == 'бот,' and message.text[-1] == '?':
 		if randint(0,1) == 1:
 			bot.send_message(message.chat.id, 'Да')
