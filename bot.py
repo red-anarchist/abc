bb79e2ad52a005d41a4334cf15e15adbd40d6c67
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2020-03-07 16:33:55 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot('1032705208:AAHL8Ut4k7iybsTEMszWVFB2JA8_Hw7GsBQ')
 def get_text_messages(message):
 	if message.text == 'Удивите меня':
 		bot.delete_message(message.chat.id, message.message_id)
-	elif message.text[1:] == 'Игры':
+	elif message.text[2:] == 'Игры':
 		bot.delete_message(message.chat.id, message.message_id)
 	elif message.text == 'Связаться с поддержкой':
 		bot.delete_message(message.chat.id, message.message_id)
