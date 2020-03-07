4b911cbf3b2d5c1a562cd4bc138b8d8ef95f8419
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2020-03-07 16:31:09 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from random import randint
 
 bot = telebot.TeleBot('1032705208:AAHL8Ut4k7iybsTEMszWVFB2JA8_Hw7GsBQ')
 
@@ -7,7 +8,16 @@ bot = telebot.TeleBot('1032705208:AAHL8Ut4k7iybsTEMszWVFB2JA8_Hw7GsBQ')
 def get_text_messages(message):
 	if message.text == 'Удивите меня':
 		bot.delete_message(message.chat.id, message.message_id)
+	elif message.text[1:] == 'Игры':
+		bot.delete_message(message.chat.id, message.message_id)
+	elif message.text == 'Связаться с поддержкой':
+		bot.delete_message(message.chat.id, message.message_id)
 	elif message.text.lower() == 'del' and (message.from_user.username == 'ever_soup' or message.from_user.username == '@ever_soup'):
 		bot.delete_message(message.chat.id, message.reply_to_message.id)
+	elif message.text[:3].lower() == 'бот,' and message.text[-1] == '?':
+		if randint(0,1) == 1:
+			bot.send_message(message.chat.id, 'Да')
+		else:
+			bot.send_message(message.chat.id, 'Нет')
 
 bot.polling(none_stop=True)
