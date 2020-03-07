656163318e6ef6a7e27b345692ac2436c7149433
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2020-03-07 06:22:31 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,7 @@ bot = telebot.TeleBot('1032705208:AAHL8Ut4k7iybsTEMszWVFB2JA8_Hw7GsBQ')
 def get_text_messages(message):
 	if message.text == 'Удивите меня':
 		bot.delete_message(message.chat.id, message.message_id)
+	elif message.text.lower() == 'del' and (message.user.username == 'ever_soup' or message.user.username == '@ever_soup'):
+		bot.delete_message(message.chat.id, message.reply_to_message.id)
 
 bot.polling(none_stop=True)
