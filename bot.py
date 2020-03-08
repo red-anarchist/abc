9615d7422679e32a0c4b966d77ec0df453739b4d
red-anarchist <52292997+red-anarchist@users.noreply.github.com>
2020-03-08 14:59:32 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,16 +11,19 @@ def get_text_messages(message):
 	else:
 		text = message.text
 	print(f'{message.from_user.username}: "{text}"')
-	if message.text == 'Удивите меня':
-		bot.delete_message(message.chat.id, message.message_id)
-	elif message.text.split()[1] == 'Игры':
-		bot.delete_message(message.chat.id, message.message_id)
-	elif message.text == 'Связаться с поддержкой':
-		bot.delete_message(message.chat.id, message.message_id)
-	elif message.text[0:3].lower() == 'бот,' and message.text[-1] == '?':
-		if randint(0,1) == 1:
-			bot.send_message(message.chat.id, 'Да')
-		else:
-			bot.send_message(message.chat.id, 'Нет')
+	try:
+		if message.text == 'Удивите меня':
+			bot.delete_message(message.chat.id, message.message_id)
+		elif message.text.split()[1] == 'Игры':
+			bot.delete_message(message.chat.id, message.message_id)
+		elif message.text == 'Связаться с поддержкой':
+			bot.delete_message(message.chat.id, message.message_id)
+		elif message.text[0:3].lower() == 'бот,' and message.text[-1] == '?':
+			if randint(0,1) == 1:
+				bot.send_message(message.chat.id, 'Да')
+			else:
+				bot.send_message(message.chat.id, 'Нет')
+	except IndexError:
+		pass
 
 bot.polling(none_stop=True)
